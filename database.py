0565a6ab5dbbf4e578a2c1bf957bb79a1808e8c0
idumahg <goi@case.edu>
2022-09-13 02:21:16 -0400

    New updates

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-"""A database encapsulating collections of near-Earth objects and their close approaches.
+"""A database encapsulating near-Earth objects and their close approaches.
 
 A `NEODatabase` holds an interconnected data set of NEOs and close approaches.
 It provides methods to fetch an NEO by primary designation or by name, as well
@@ -9,8 +9,8 @@ Under normal circumstances, the main module creates one NEODatabase from the
 data on NEOs and close approaches extracted by `extract.load_neos` and
 `extract.load_approaches`.
 
-You'll edit this file in Tasks 2 and 3.
 """
+
 
 class NEODatabase:
     """A database of near-Earth objects and their close approaches.
@@ -20,45 +20,43 @@ class NEODatabase:
     help fetch NEOs by primary designation or by name and to help speed up
     querying for close approaches that match criteria.
     """
+
     def __init__(self, neos, approaches):
         """Create a new `NEODatabase`.
 
-        As a precondition, this constructor assumes that the collections of NEOs
-        and close approaches haven't yet been linked - that is, the
+        As a precondition, this constructor assumes that the collections
+        of NEOs and close approaches haven't yet been linked - that is, the
         `.approaches` attribute of each `NearEarthObject` resolves to an empty
         collection, and the `.neo` attribute of each `CloseApproach` is None.
 
         However, each `CloseApproach` has an attribute (`._designation`) that
         matches the `.designation` attribute of the corresponding NEO. This
-        constructor modifies the supplied NEOs and close approaches to link them
-        together - after it's done, the `.approaches` attribute of each NEO has
-        a collection of that NEO's close approaches, and the `.neo` attribute of
-        each close approach references the appropriate NEO.
+        constructor modifies the supplied NEOs and close approaches to link
+        them together - after it's done, the `.approaches` attribute of each
+        NEO has a collection of that NEO's close approaches, and the `.neo`
+        attribute of each close approach references the appropriate NEO.
 
         :param neos: A collection of `NearEarthObject`s.
         :param approaches: A collection of `CloseApproach`es.
         """
         self._neos = neos
         self._approaches = approaches
-        
+
+        # Was lost on how structure the __init__, got the idea from someone's
+        # work posted on Knowledge
         self.neos_name = {}
         self.neos_designation = {}
-        
+
         # Link neos with their name and designation.
         for neo in neos:
             if neo.name:
                 self.neos_name[neo.name] = neo
-            self.neos_designation[neo.designation] = neo            
-        
+            self.neos_designation[neo.designation] = neo
+
         for approach in approaches:
-            neo = self.neos_designation[approach.designation]
+            neo = self.neos_designation[approach._designation]
             neo.approaches.append(approach)
             approach.neo = neo
-            
-            
-        # TODO: What additional auxiliary data structures will be useful?
-
-        # TODO: Link together the NEOs and their close approaches.
 
     def get_neo_by_designation(self, designation):
         """Find and return an NEO by its primary designation.
@@ -71,11 +69,11 @@ class NEODatabase:
         match is found.
 
         :param designation: The primary designation of the NEO to search for.
-        :return: The `NearEarthObject` with the desired primary designation, or `None`.
+        :return: The `NearEarthObject` with the desired primary designation,
+        or `None`.
         """
-        # TODO: Fetch an NEO by its primary designation.
         return self.neos_designation.get(designation)
-            
+
     def get_neo_by_name(self, name):
         """Find and return an NEO by its name.
 
@@ -90,24 +88,23 @@ class NEODatabase:
         :param name: The name, as a string, of the NEO to search for.
         :return: The `NearEarthObject` with the desired name, or `None`.
         """
-        # TODO: Fetch an NEO by its name.
         return self.neos_name.get(name)
-            
-    def query(self, filters=()):
-        """Query close approaches to generate those that match a collection of filters.
 
-        This generates a stream of `CloseApproach` objects that match all of the
-        provided filters.
+    def query(self, filters=()):
+        """Query close approaches to generate matches with given filters.
+
+        This generates a stream of `CloseApproach` objects that match
+        all of the provided filters.
 
         If no arguments are provided, generate all known close approaches.
 
-        The `CloseApproach` objects are generated in internal order, which isn't
-        guaranteed to be sorted meaninfully, although is often sorted by time.
+        The `CloseApproach` objects are generated in internal order,
+        which isn't guaranteed to be sorted meaningfully,
+        although is often sorted by time.
 
-        :param filters: A collection of filters capturing user-specified criteria.
+        :param filters: A collection of filters capturing user's criteria.
         :return: A stream of matching `CloseApproach` objects.
         """
-        # TODO: Generate `CloseApproach` objects that match all of the filters.
         for approach in self._approaches:
             flag = True
             for f in filters:
--- write.py
+++ write.py
@@ -17,26 +17,40 @@ import json
 def write_to_csv(results, filename):
     """Write an iterable of `CloseApproach` objects to a CSV file.
 
-    The precise output specification is in `README.md`. Roughly, each output row
-    corresponds to the information in a single close approach from the `results`
-    stream and its associated near-Earth object.
+    The precise output specification is in `README.md`. Roughly, each output
+    row corresponds to the information in a single close approach from the
+    `results` stream and its associated near-Earth object.
 
     :param results: An iterable of `CloseApproach` objects.
-    :param filename: A Path-like object pointing to where the data should be saved.
+    :param filename: A Path-like object pointing to where data should be saved.
     """
-    fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
+    fieldnames = (
+        'datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name',
+        'diameter_km', 'potentially_hazardous')
+    with open(filename, 'w') as outfile:
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for result in results:
+            # Idea gotten from Knowledge
+            result_dict = {**result.serialize, **result.neo.serialize}
+            writer.writerow(result_dict)
 
 
 def write_to_json(results, filename):
     """Write an iterable of `CloseApproach` objects to a JSON file.
 
-    The precise output specification is in `README.md`. Roughly, the output is a
-    list containing dictionaries, each mapping `CloseApproach` attributes to
-    their values and the 'neo' key mapping to a dictionary of the associated
-    NEO's attributes.
+    The precise output specification is in `README.md`. Roughly, the output
+    is a list containing dictionaries, each mapping `CloseApproach` attributes
+    to their values and the 'neo' key mapping to a dictionary of the
+    associated NEO's attributes.
 
     :param results: An iterable of `CloseApproach` objects.
-    :param filename: A Path-like object pointing to where the data should be saved.
+    :param filename: A Path-like object pointing to where data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+    with open(filename, 'w') as outfile:
+        # Idea gotten from Knowledge
+        output = []
+        for result in results:
+            result_dict = {**result.serialize, 'neo': result.neo.serialize}
+            output.append(result_dict)
+        json.dump(output, outfile, indent=2)
